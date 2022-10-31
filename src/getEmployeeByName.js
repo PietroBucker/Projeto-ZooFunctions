d8df20add684311f6f1b4a534c2d91a8d8725896
Pietro Bucker <pietro_177@hotmail.com>
2022-10-31 18:29:33 -0300

    Requisito 3 feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return employeeName === undefined ? {} : employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
 }
-
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
