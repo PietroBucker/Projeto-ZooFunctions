3cdb91564dc3d6b231966b9e95329ae64bf68286
Pietro Bucker <pietro_177@hotmail.com>
2022-10-31 17:52:17 -0300

    Requisito 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((specie) => animal === specie.name)
+    .residents.every((resident) => resident.age >= age);
 }
-
+console.log(getAnimalsOlderThan('lions', 7));
 module.exports = getAnimalsOlderThan;
