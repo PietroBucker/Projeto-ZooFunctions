b296018bf6dd63ee5941c52fbdd10e3c636d6d74
Pietro Bucker <pietro_177@hotmail.com>
2022-10-31 19:51:04 -0300

    Requisito 4 feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
-
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+function getRelatedEmployees(menegerId) {
+  if (isManager(menegerId) === true) {
+    return employees.filter((employee) => employee.managers.includes(menegerId))
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
