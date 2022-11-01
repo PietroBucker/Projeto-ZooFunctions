55c056d7b3e3248c570940e7e61a87c3808c885d
Pietro Bucker <pietro_177@hotmail.com>
2022-11-01 16:22:58 -0300

    Requisito 7 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { prices: { child, adult, senior } } = data;
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.reduce((acc, curr) => (curr.age < 18 ? acc + 1 : acc), 0),
+    adult: entrants.reduce((acc, curr) => (curr.age >= 18 && curr.age < 50 ? acc + 1 : acc), 0),
+    senior: entrants.reduce((acc, curr) => (curr.age >= 50 ? acc + 1 : acc), 0),
+  };
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  return (child * countEntrants(entrants).child) + (adult * countEntrants(entrants).adult) + (senior * countEntrants(entrants).senior);
 }
-
+console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
