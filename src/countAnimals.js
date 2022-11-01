6741e0358b70dd8cb5a10a0ae5660094dec43632
Pietro Bucker <pietro_177@hotmail.com>
2022-10-31 22:44:13 -0300

    Requisito 5 feito, falta corrigir lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  const obj = {};
+  if (animal === undefined) {
+    species.forEach((specie) => { obj[specie.name] = specie.residents.length; });
+  } else {
+    const cont = species.find((specie) => specie.name === animal.specie)
+      .residents.filter((resident) => {
+        if (resident.sex === animal.sex) return resident;
+        if (undefined === animal.sex) return resident;
+      }).length;
+    return cont;
+  }
+  return obj;
+}
+console.log(countAnimals({ specie: 'penguins' }));
 module.exports = countAnimals;
