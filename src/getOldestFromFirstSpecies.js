64e3db04ffffaf64c0a07023999a5509c4cec5c4
Pietro Bucker <pietro_177@hotmail.com>
2022-11-03 15:16:55 -0300

    Requisito 10 feito, lint com 1 erro

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+const [, Burl] = employees;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const espeicesId = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animal = species.find((specie) => specie.id === espeicesId)
+    .residents.reduce((acc, curr) => curr.age > acc.age ? acc = curr : acc);
+  const { name, sex, age } = animal;
+  return [name, sex, age];
 }
 
+console.log(getOldestFromFirstSpecies(Burl.id));
 module.exports = getOldestFromFirstSpecies;
