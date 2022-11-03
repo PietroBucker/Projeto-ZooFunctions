b4d362cfc87b9939ef19024c5b441ca9508a569c
Pietro Bucker <pietro_177@hotmail.com>
2022-11-03 17:42:39 -0300

    Requisito 11 incompleto

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,24 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(param) {
+  if (!param) {
+    return employees.map((employee) => ({ id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: employee.responsibleFor,
+      locations: species.filter((specie) => employee.responsibleFor.includes(specie.id))
+        .map((teste) => teste.location),
+    }));
+  }
+  if (employees.some((employee) => employee.firstName === param)) {
+    const employeeInfo = employees.find((employee) => (employee.firstName === param));
+    return { id: employeeInfo.id,
+      fullName: `${employeeInfo.firstName} ${employeeInfo.lastName}`,
+      species: employeeInfo.responsibleFor,
+      locations: species.filter((specie) => employeeInfo.responsibleFor.includes(specie.id))
+        .map((teste) => teste.location),
+    };
+  } throw new Error('Informações inválidas');
 }
-
+// console.log(getEmployeesCoverage('Sharonda'));
 module.exports = getEmployeesCoverage;
