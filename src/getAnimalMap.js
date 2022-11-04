843e05d907995ca0bd8ea207eaf76af7f8fe4372
Pietro Bucker <pietro_177@hotmail.com>
2022-11-04 16:10:00 -0300

    Requisito 14 completo

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -11,18 +11,54 @@ const getObj = () => {
   });
   return obj;
 };
-const getObjBySex = () => {
-  return getObj();
+
+const getNameResid = (param) => {
+  const obj = {};
+  arrLocations.forEach((arrLocation) => {
+    obj[arrLocation] = species.filter((specie) => specie.location === arrLocation)
+      .map((specieName) => {
+        if (param.sorted) {
+          return {
+            [specieName.name]: specieName.residents.map((test) => test.name).sort(),
+          };
+        }
+        return {
+          [specieName.name]: specieName.residents.map((test) => test.name),
+        };
+      });
+  });
+  return obj;
+};
+
+const getObjBySex = (param) => {
+  const obj = {};
+  arrLocations.forEach((arrLocation) => {
+    obj[arrLocation] = species.filter((specie) => specie.location === arrLocation)
+      .map((specieName) => {
+        if (param.sorted) {
+          return {
+            [specieName.name]: specieName.residents.filter((test) => test.sex === param.sex)
+              .map((teste) => teste.name).sort(),
+          };
+        }
+        return {
+          [specieName.name]: specieName.residents.filter((test) => test.sex === param.sex)
+            .map((teste) => teste.name),
+        };
+      });
+  });
+  return obj;
 };
 function getAnimalMap(options) {
-  if (options.includeNames) {
+  if (!options || !options.includeNames) {
     return getObj();
   }
-  if (options.sex) {
-    return getObj();
+  if (options.includeNames && !options.sex) {
+    return getNameResid(options);
   }
+  return getObjBySex(options);
 }
-console.log(getObj());
-// console.log(getObjBySex());
-// console.log(getAnimalMap({ includeNames: true }));
+// console.log(getObj());
+// console.log(getObjBySex({sex: 'male', sorted: true }));
+console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 module.exports = getAnimalMap;
