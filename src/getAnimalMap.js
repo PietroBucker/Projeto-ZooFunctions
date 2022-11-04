9690333b40e048b0a15a4e82a1f89da7a8b9ce10
Pietro Bucker <pietro_177@hotmail.com>
2022-11-03 21:48:05 -0300

    Requisito 14 incompleto

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,14 +1,28 @@
 const data = require('../data/zoo_data');
 
+const arrLocations = ['NE', 'NW', 'SE', 'SW'];
 const { species } = data;
+
+const getObj = () => {
+  const obj = {};
+  arrLocations.forEach((arrLocation) => {
+    obj[arrLocation] = species.filter((specie) => specie.location === arrLocation)
+      .map((specieName) => specieName.name);
+  });
+  return obj;
+};
+const getObjBySex = () => {
+  return getObj();
+};
 function getAnimalMap(options) {
-  return {
-    NE: species.filter((specie) => specie.location === 'NE').map((teste) => {
-      return {
-        [teste.name]: ,
-      };
-    }),
-  };
+  if (options.includeNames) {
+    return getObj();
+  }
+  if (options.sex) {
+    return getObj();
+  }
 }
-console.log(getAnimalMap());
+console.log(getObj());
+// console.log(getObjBySex());
+// console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,15 +1,15 @@
 const data = require('../data/zoo_data');
 
 const { employees, species } = data;
-const [, Burl] = employees;
+// const [, Burl] = employees;
 
 function getOldestFromFirstSpecies(id) {
   const espeicesId = employees.find((employee) => employee.id === id).responsibleFor[0];
   const animal = species.find((specie) => specie.id === espeicesId)
-    .residents.reduce((acc, curr) => curr.age > acc.age ? acc = curr : acc);
+    .residents.reduce((acc, curr) => (curr.age > acc.age ? acc = curr : acc));
   const { name, sex, age } = animal;
   return [name, sex, age];
 }
 
-console.log(getOldestFromFirstSpecies(Burl.id));
+// console.log(getOldestFromFirstSpecies(Burl.id));
 module.exports = getOldestFromFirstSpecies;
